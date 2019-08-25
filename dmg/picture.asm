1897aa3035767a723e952d497ea721a8b9375cf4
tmk <tmk@tuta.io>
2019-08-25 15:59:27 +0200

    display picture example

---
+++ dmg/picture.asm
@@ -0,0 +1,141 @@
+; display picture composed of 242 unique tiles by tmk @ https://github.com/gitendo/
+; Gameboy Horror pixeled by vassink @ http://pixeljoint.com/pixelart/61800.htm
+
+	INCLUDE "hardware.inc"			; system defines
+
+	SECTION	"Start",ROM0[$100]		; start vector, followed by header data applied by rgbfix.exe
+	nop
+	jp	start
+
+        SECTION "Example",ROM0[$150]		; code starts here
+
+start:
+	di					; disable interrupts
+	ld	sp,$E000			; setup stack
+
+.wait_vbl					; wait for vblank to properly disable lcd
+	ld	a,[rLY]	
+	cp	$90
+	jr	nz,.wait_vbl
+
+	xor	a				; reset important registers
+	ld	[rIF],a
+	ld	[rLCDC],a
+	ld	[rSTAT],a
+	ld	[rSCX],a
+	ld	[rSCY],a
+	ld	[rLYC],a
+	ld	[rIE],a
+
+	ld	hl,_RAM                         ; clear ram (fill with a which is 0 here)
+	ld	bc,$2000-2			; watch out for stack ;)
+	call	fill
+
+	ld	hl,_HRAM			; clear hram
+	ld	c,$80				; a = 0, b = 0 here, so let's save a byte and 4 cycles (ld c,$80 - 2/8 vs ld bc,$80 - 3/12)
+	call	fill
+						; no point in clearing vram, we'll overwrite it with picture data anyway
+						; lcdc is already disabled so we have 'easy' access to vram
+
+	ld	hl,picture_chr			; picture data
+	ld	de,_VRAM			; place it between $8000-8FFF (tiles are numbered here from 0 to 255)
+	ld	bc,3872				; gbhorror.chr file size
+	call 	copy
+
+	ld	hl,picture_map			; picture map (not padded, 160x144px = 20*18)
+	ld	de,_SCRN0			; place it at $9800
+	call	copy_map			; should have used bmp2cgb with -e option :)
+
+	ld	a,%00011011			; bits: 7-6 = 1st color, 5-4 = 2nd, 3-2 = 3rd and 1-0 = 4th color
+						; color values: 00 - light, 01 - gray, 10 - dark gray, 11 - dark
+	ld	[rBGP],a			; bg palette
+	ld	[rOBP0],a			; obj palettes (not used in this example)
+	ld	[rOBP1],a
+	
+	ld	a,LCDCF_ON | LCDCF_BG8000 | LCDCF_BG9800 | LCDCF_OBJ8 | LCDCF_OBJOFF | LCDCF_WINOFF | LCDCF_BGON
+						; lcd setup: tiles at $8000, map at $9800, 8x8 sprites (disabled), no window, etc.
+	ld	[rLCDC],a			; enable lcd
+
+.the_end
+	halt					; save battery
+;	nop					; nop after halt is mandatory but rgbasm takes care of it :)
+	jr	.the_end			; endless loop
+
+
+;-------------------------------------------------------------------------------	
+copy:
+;-------------------------------------------------------------------------------
+; hl - source address
+; de - destination
+; bc - size
+
+	inc	b
+	inc	c
+	jr	.skip
+.copy
+	ld	a,[hl+]
+	ld	[de],a
+	inc	de
+.skip
+	dec	c
+	jr	nz,.copy
+	dec	b
+	jr	nz,.copy
+	ret
+
+
+;-------------------------------------------------------------------------------	
+copy_map:
+;-------------------------------------------------------------------------------
+; hl - map data
+; de - _SCRN0 or _SCRN1
+; b  - rows
+; c  - columns
+						; picture is 160x144px,
+	ld	b,18				; 144/8 = 18 rows
+.next_row
+	ld	c,20				; 160/8 = 20 tiles each
+.row
+	ld	a,[hl+]				; fetch one byte from rom and increase hl to point to another one
+	ld	[de],a				; store it at _SCRN0
+	inc	de				; unfortunately there's no [de+]
+	dec	c				; one byte done
+	jr	nz,.row				; next byte, copy untill c=0
+
+	ld	a,e				; our row = 20 which is what you can see on the screen
+	add	a,12				; the part you don't see is 12, so we need to add it
+	jr	nc,.skip			; to make sure the next row is copied at right offset
+	inc	d                               ; nc flag is set when a+12 > 255
+.skip
+	ld	e,a
+
+	dec	b				; next row, copy untill b=0
+	jr	nz,.next_row
+	ret
+
+
+;-------------------------------------------------------------------------------
+fill:
+;-------------------------------------------------------------------------------
+; a - byte to fill with
+; hl - destination address
+; bc - size of area to fill
+
+	inc	b
+	inc	c
+	jr	.skip
+.fill
+	ld	[hl+],a
+.skip
+	dec	c
+	jr	nz,.fill
+	dec	b
+	jr	nz,.fill
+	ret
+
+;-------------------------------------------------------------------------------
+
+picture_chr:					; bmp2cgb -x -y -z gbhorror.bmp
+        INCBIN	"gbhorror.chr"
+picture_map:
+	INCBIN	"gbhorror.map"
